1ce13b5866c4e4a67a07afc72a1a0ac4fc6ad0e2
JandersonSR <45982396+JandersonSR@users.noreply.github.com>
2020-09-23 22:59:50 -0300

    Update media.js
    Correção das variáveis para manipulação dos calculos, incrementado propriedade .value

--- atividade5/media.js
+++ atividade5/media.js
@@ -1,17 +1,17 @@
 function res(){
-            var nome = document.getElementById("addNome");
-            alert(addNome);
-            var score1 = document.getElementById('addNota1');
-            var score2 = document.getElementById('addNota2');
-            var score3 = document.getElementById('addNota3');
-            var media = parseFloat((parseFloat(score1) + parseFloat(score2) + parseFloat(score3))/3).toFixed(2);
+            var nome = document.getElementById("addNome").value;
+           
+            var score1 = document.getElementById("addNota1").value;
+            var score2 = document.getElementById('addNota2').value;
+            var score3 = document.getElementById('addNota3').value;
+           var media = parseFloat((parseFloat(score1) + parseFloat(score2) + parseFloat(score3))/3).toFixed(2);
         
-            alert("Sua media é:" + " " + media);
+            document.getElementById('resultado').innerHTML = media;
             
         }
         
     /*  Metodo 2
-     var nome = prompt("Qual é seu nome?");
+        var nome = prompt("Qual é seu nome?");
         var score1 = prompt("Digite a nota 1");
         var score2 = prompt("Digite a nota 2");
         var score3 = prompt("Digite a nota 3");
