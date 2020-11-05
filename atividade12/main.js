9bf66afe774fc7d9f012b02494d6d351b2073f88
JandersonSR <45982396+JandersonSR@users.noreply.github.com>
2020-11-04 22:29:06 -0300

    Atividade12

---
+++ atividade12/main.js
@@ -0,0 +1,9 @@
+var escolha1 = document.getElementById("escolha1");
+var escolha2 = document.getElementById("escolha2");
+
+escolha1.onclick = function() {  
+        alert( "Volte sempre a esta página");
+}
+escolha2.onclick = function(){
+    alert("Que bom que você visitou a nossa página");
+}
